ef815710000f65d1588f2b8c45700b5f5a78919e
Adoolover <Adoolover@users.noreply.github.com>
2018-09-17 14:11:26 -0600

    re

--- ineractive sceane/sketch.js
+++ ineractive sceane/sketch.js
@@ -9,8 +9,11 @@
 
 let squareX = 0;
 let squareY = 0;
-let squareWidth = 50
-let squareHeight = 50
+let squareYG2 = -150;
+let squareYG3 = -300;
+let squareYG4 = -450
+let squareWidth = 50;
+let squareHeight = 50;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -19,6 +22,7 @@ function setup() {
 
 function draw(){
   background(255);
+
   rect(squareX, squareY, squareWidth, squareHeight);
   rect(squareX + 200 , squareY, squareWidth, squareHeight);
   rect(squareX + 400 , squareY, squareWidth, squareHeight);
@@ -29,24 +33,45 @@ function draw(){
   rect(squareX + 1400 , squareY, squareWidth, squareHeight);
   rect(squareX + 1600 , squareY, squareWidth, squareHeight);
 
-  rect(squareX + 1500 , squareY -150, squareWidth, squareHeight);
-  rect(squareX + 1300 , squareY -150, squareWidth, squareHeight);
-  rect(squareX + 1100 , squareY -150, squareWidth, squareHeight);
-  rect(squareX + 900 , squareY - 150, squareWidth, squareHeight);
-  rect(squareX + 700 , squareY - 150, squareWidth, squareHeight);
-  rect(squareX + 500 , squareY - 150, squareWidth, squareHeight);
-  rect(squareX + 300 , squareY - 150, squareWidth, squareHeight);
-  rect(squareX + 100 , squareY - 150, squareWidth, squareHeight);
+  rect(squareX + 1500 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 1300 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 1100 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 900 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 700 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 500 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 300 , squareYG2, squareWidth, squareHeight);
+  rect(squareX + 100 , squareYG2, squareWidth, squareHeight);
 
-  rect(squareX, squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 200 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 400 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 600 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 800 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 1000 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 1200 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 1400 , squareY - 300, squareWidth, squareHeight);
-  rect(squareX + 1600 , squareY - 300, squareWidth, squareHeight);
+  rect(squareX, squareYG3, squareWidth, squareHeight);
+  rect(squareX + 200 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 400 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 600 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 800 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 1000 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 1200 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 1400 , squareYG3, squareWidth, squareHeight);
+  rect(squareX + 1600 , squareYG3, squareWidth, squareHeight);
 
-  squareY++
+  rect(squareX + 1500 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 1300 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 1100 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 900 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 700 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 500 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 300 , squareYG4, squareWidth, squareHeight);
+  rect(squareX + 100 , squareYG4, squareWidth, squareHeight);
+
+  squareY++;
+  squareYG2++;
+  squareYG3++;
+  if (squareY >= windowHeight){
+    squareY = 0;
+  }
+  if (squareYG2 >= windowHeight){
+    squareYG2 = 0;
+  }
+  if (squareYG3 >= windowHeight){
+    squareYG3 = 0;
+  }
+
 }
