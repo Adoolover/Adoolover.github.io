1d31cdce35dd7c6bd58aed61f365f3aaa0fdfbf9
Adoolover <Adoolover@users.noreply.github.com>
2018-09-28 14:11:34 -0600

    new

--- Stateproject/sketch.js
+++ Stateproject/sketch.js
@@ -10,40 +10,84 @@ let lineX1, lineX2;
 let lineY1, lineY2;
 let circleWidth;
 let circY, circX;
+let girdPeace;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  lineX1 = 10;
-  lineX2 = 60;
-  lineY1 = 10;
-  lineY2 = 60;
-  circY = 50;
-  circX = 50;
-  circleWidth = 50;
+  lineX1 = 140;
+  lineX2 = 340;
+  lineY1 = 30;
+  lineY2 = 230;
+  circY = height/2;
+  circX = width/2;
+  circleWidth = 200;
+  girdPeace = 0;
 }
 
 function draw() {
+  strokeWeight(1);
+
   drawBoard();
   drawCircle();
-}
-function drawBoard(){
-  line(width*0.25,0,width/4,height);
-  line(widht*0.75,y1,x2,y2);
-  line(x1,y1,x2,y2);
-  line(x1,y1,x2,y2);
-}
-function mousePressed(){
   drawX();
 }
-function drawX(){
-  for(let i = 0; i < 2; i++){
-    line(lineX1,lineY1,lineX2,lineY2);
-    lineX1 += 50;
-    lineX2 -= 50;
+function mousePressed(){
+  findGrid();
+}
+function findGrid(){
+
+  if (mouseX < width*0.33 && mouseY < height*0.33){
+    girdPeace = 1;
   }
-  lineX1 = 10;
-  lineX2 = 60;
+  else if (mouseX < width*0.66 && mouseX > width*0.33 && mouseY < height*0.33){
+    girdPeace = 2;
+  }
+  else if (mouseX > width*0.66 && mouseY < height*0.33){
+    girdPeace = 3;
+  }
+  else if (mouseX < width*0.33 && mouseY > height*0.33 && mouseY < height*0.66){
+    girdPeace = 4;
+  }
+  else if (mouseX > width*0.33 && mouseX < width*0.66 && mouseY > height*0.33 && mouseY < height*0.66){
+    girdPeace = 5;
+  }
+  else if (mouseX > width*0.66 && mouseY > height*0.33 && mouseY < height*0.66){
+    girdPeace = 6;
+  }
+  else if (mouseX < width*0.33 && mouseY < height*0.33){
+    girdPeace = 7;
+  }
+  else if (mouseX < width*0.33 && mouseY < height*0.33){
+    girdPeace = 8;
+  }
+  else if (mouseX < width*0.33 && mouseY < height*0.33){
+    girdPeace = 9;
+  }
+}
+
+function drawBoard(){
+  line(width*0.33,0,width*0.33,height);
+  line(width*0.66,0,width*0.66,height);
+  line(0,height*0.33,width, height*0.33);
+  line(0,height*0.66,width,height*0.66);
+}
+
+
+
+function drawX(){
+  if (girdPeace === 6){
+    strokeWeight(10);
+    for(let i = 0; i < 2; i++){
+      line(lineX1,lineY1,lineX2,lineY2);
+      lineX1 += 200;
+      lineX2 -= 200;
+    }
+  }
+  lineX1 = 100;
+  lineX2 = 300;
+
 }
 
 function drawCircle(){
+  fill(0);
   ellipse(circX,circY,circleWidth);
 }
