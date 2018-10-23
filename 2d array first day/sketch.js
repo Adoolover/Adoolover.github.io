6ce053172aa610c299dbb28ed4415d9c1bcd16d5
Adoolover <Adoolover@users.noreply.github.com>
2018-10-23 14:04:09 -0600

    Day one 2d arrays

---
+++ 2d array first day/sketch.js
@@ -0,0 +1,44 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let grid = [];
+let cols = 100;
+let rows = 100;
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+}
+function draw() {
+  stroke(0);
+
+
+}
+
+function mousePressed(){
+  for (let i = 0; i < cols; i++){
+    grid[i] = [];
+    for (let j = 0; j < rows; j++){
+      grid[i][j] = int(random(2));
+    }
+  }
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      let h = height/cols;
+      let w = width/rows;
+      if (grid[i][j] === 0){
+        fill(125);
+      }
+      else {
+        fill(255);
+      }
+      rect(j*w, i*h, w, h);
+
+    }
+  }
+}
