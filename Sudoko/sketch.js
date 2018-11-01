3202749e415c6901f91e71994c8cd9e4e0be7b48
Adoolover <Adoolover@users.noreply.github.com>
2018-11-01 14:16:08 -0600

    update

--- Sudoko/sketch.js
+++ Sudoko/sketch.js
@@ -10,13 +10,18 @@ let rows = 9;
 let cols = 9;
 let cellSize;
 
+function preload(){
+  grid = loadStrings("assets/sudoku1.txt");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   cellSize = height/cols;
   textAlign(CENTER,CENTER);
-  grid = create2dArray();
+  textSize(25);
   interactingGrid = createEmptyArray();
+  cleanUpTheGrid();
 }
 
 function draw() {
@@ -26,18 +31,26 @@ function draw() {
 
 }
 
+function cleanUpTheGrid(){
+  for (let i = 0; i < grid.length; i++){
+    grid[i] = grid[i].split("");
+  }
+}
+
 function keyPressed(){
   for (let y = 0; y < rows;y++){
     for (let x = 0; x < cols; x++){
       if (interactingGrid[y][x] === 1){
         grid[y][x] = key;
       }
+      interactingGrid[y][x] = 0;
     }
   }
   // if (key === 50){
   //   return 2;
   // }
 }
+
 function mousePressed(){
   let x = floor((mouseX-width*0.25) / cellSize);
   let y = floor(mouseY / cellSize);
@@ -49,7 +62,9 @@ function mousePressed(){
     interactingGrid[y][x] = 0;
   }
 
+
 }
+
 function drawlines(){
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
@@ -75,40 +90,35 @@ function displayGrid() {
     for (let x = 0; x < cols; x++) {
       fill(255);
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
+      for (let i = 0; i < rows+1; i++){
+        let string = str(i);
+        if (grid[y][x] === "0"){
+          fill("black");
+          text(" ",x*cellSize + cellSize/2, y*cellSize + cellSize/2);
+          fill("black");
+        }
+        else  if (grid[y][x] === string){
+          fill("black");
+          text(i,x*cellSize +  cellSize/2, y*cellSize + cellSize/2);
+          fill("black");
+        }
+      }
 
-      if (grid[y][x] === 0){
-        fill("orange");
-        text("hi",x*cellSize + cellSize/2, y*cellSize + cellSize/2);
-        fill("black");
-      }
-      else  if (grid[y][x] === 1){
-        fill("pink");
-        text("hi",x*cellSize +  cellSize/2, y*cellSize + cellSize/2);
-        fill("black");
-      }
-      else  if (grid[y][x] === "2"){
-        fill("black");
-        text("2",x*cellSize +  cellSize/2, y*cellSize + cellSize/2);
-        fill("black");
-      }
     }
   }
 }
+
 function create2dArray(){
   let arr = [];
   for(let y = 0; y < rows; y++ ) {
     arr.push([]);
     for(let x = 0; x < cols; x++){
-      if (random(100) < 50) {
-        arr[y].push(0);
-      }
-      else {
-        arr[y].push(1);
-      }
+      arr[y].push(0);
     }
   }
   return arr;
 }
+
 function createEmptyArray(){
   let arr = [];
   for(let y = 0; y < rows; y++ ) {
