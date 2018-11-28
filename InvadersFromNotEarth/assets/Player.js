fb8d3f86b2f6e09ac9dd680b42b8cfa91f9a82ca
Adoolover <Adoolover@users.noreply.github.com>
2018-11-28 14:12:15 -0600

    ahhhh

--- InvadersFromNotEarth/assets/Player.js
+++ InvadersFromNotEarth/assets/Player.js
@@ -1,5 +1,5 @@
 // Cody Flynn
-// Nov. _ _ , 2018
+// Nov. 24, 2018
 
 // Creating the player and some of its propertys
 class Player {
@@ -22,27 +22,9 @@ class Player {
   }
 
   display() {
-    // // displaying the player and checking the health
-    // if (this.health === 0){
-    //   tint(255,0);
-    //   image(this.img, this.x, this.y, this.size, this.size);
-    //   this.deathOccured = true;
-    // }
-    // else{
-    //   tint(255, 255);
-    image(this.img, this.x, this.y, this.size, this.size);
-    // }
 
-    // // displaying text after death of player
-    // if (this.deathOccured){
-    //   fill(255);
-    //   text("Press Space to continue", width/2, height/2);
-    //   if (keyIsDown(32)){
-    //     this.lives -= 1;
-    //     this.health = 5;
-    //     this.deathOccured = !this.deathOccured;
-    //   }
-    // }
+
+    image(this.img, this.x, this.y, this.size, this.size);
 
     // displaying lives
     fill("white");
@@ -52,7 +34,6 @@ class Player {
       let x = this.size*0.25*i;
       image(this.img, width*0.99 - x, height*0.05, this.size*0.25, this.size*0.25);
     }
-
     for (let i = this.projectiles.length-1; i >= 0; i--) {
       this.projectiles[i].move();
       if (this.projectiles[i].hitEdge()) {
@@ -76,14 +57,21 @@ class Player {
 
   // checkcing lives
   checkHealth() {
-    if (this.lives <= 0) {
-      return true;
-    }
-
     if (this.health <= 0) {
       this.lives--;
       this.health = this.maxHealth;
       return true;
     }
   }
+  checkLives() {
+    if (this.lives <= 0) {
+      return true;
+    }
+  }
 }
+
+  healthBar() {
+    rect(width/2,height/2, this.size, this.size/4)
+    fill(255,0,0);
+    rect(width/2,height/2, this.size, this.size/4)
+  }
