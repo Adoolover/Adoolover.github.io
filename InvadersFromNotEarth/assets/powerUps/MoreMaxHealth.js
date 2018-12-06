d90142cd6addcd53545d1d9b07315b977f8c9bbc
Zetsu4 <42389794+Zetsu4@users.noreply.github.com>
2018-12-06 14:10:16 -0600

    Update MoreMaxHealth.js

--- InvadersFromNotEarth/assets/powerUps/MoreMaxHealth.js
+++ InvadersFromNotEarth/assets/powerUps/MoreMaxHealth.js
@@ -8,8 +8,8 @@ class MoreMaxHealth {
 
   pickUp(playerObj) {
     playerObj.maxHealth++;
-    playerObj.health = playerObj.maxHealth;
     playerObj.maxHealth = constrain(playerObj.maxHealth, 2, 50);
+    playerObj.health = playerObj.maxHealth;
     return playerObj;
   }
 }
